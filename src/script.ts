daa08a024a9d47377f8de1354082d417f8885f4e
Malcolm Lima <malcolmmellolima@gmail.com>
2021-10-27 21:09:15 -0300

    type em parametros

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,6 @@
-let nomes: any = ['baonieky', 'pedro', 'neymar agiota']
+function firstLetterUpperCaser(name: string) {
+    let firstLetter = name.charAt(0).toUpperCase()
+    return  firstLetter+name.substring(1)
+}
 
-nomes.push(556)
+firstLetterUpperCaser('marlo')
