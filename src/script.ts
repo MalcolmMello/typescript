f2f097a6765458c62d9425fd03e68717dd1cd25a
Malcolm Lima <malcolmmellolima@gmail.com>
2021-10-28 06:32:49 -0300

    type assertions

--- src/script.ts
+++ src/script.ts
@@ -1,18 +1,5 @@
-/*type User = {
-    nome: string,
-    idade: number
-}*/
-interface User {
-    nome: string,
-    idade: number
-}
+let idadeField = document.getElementById('idade') as HTMLInputElement
 
-function resumo(usuario: User) {
-    return `Olá ${usuario.nome}, você tem ${usuario.idade} anos`
-}
+// ...
 
-resumo({
-    nome: 'Malcolm',
-    idade: 90
-})
-
+console.log( idadeField.value )
